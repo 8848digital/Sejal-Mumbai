befa5f3045e2805f532a65b64890255b5cce930a
shyam-kumar8848 <113445741+shyam-kumar8848@users.noreply.github.com>
2024-03-28 17:26:53 +0530

    stock entry report change

--- sejal_mumbai/api/v1/stock_entry.py
+++ sejal_mumbai/api/v1/stock_entry.py
@@ -345,12 +345,12 @@ def source_warehouse_item_code(kwargs):
         SELECT sle.warehouse, sle.item_code
         FROM `tabStock Ledger Entry` AS sle
         WHERE docstatus = 1  
-        AND actual_qty = 1 
+        AND qty_after_transaction = 1 
         AND (sle.warehouse, sle.item_code) NOT IN (
             SELECT warehouse, item_code
             FROM `tabStock Ledger Entry`
             WHERE docstatus = 1  
-            AND actual_qty = -1 
+            AND qty_after_transaction = 0
         )
         {conditions}
         """,
